b0e03580b0e9be60ed936d0202fad6fae9913472
Xinye Chen <xchen4@goldengate.cs.brynmawr.edu>
2023-03-22 21:30:02 -0400

    A08 complete

--- A08/single_mandelbrot.c
+++ A08/single_mandelbrot.c
@@ -1,3 +1,9 @@
+/*----------------------------------------------
+ * Author: Parika Chen
+ * Date: 2023/3/22
+ * This program outputs a PPM image of the mandelbrot set.
+ ---------------------------------------------*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -5,8 +11,12 @@
 #include <sys/time.h>
 #include "read_ppm.h"
 #include "write_ppm.h"
+#include <string.h>
 
 int main(int argc, char* argv[]) {
+
+  struct timeval tstart, tend;
+  
   int size = 480;
   float xmin = -2.0;
   float xmax = 0.47;
@@ -29,10 +39,76 @@ int main(int argc, char* argv[]) {
   printf("  X range = [%.4f,%.4f]\n", xmin, xmax);
   printf("  Y range = [%.4f,%.4f]\n", ymin, ymax);
 
-  // todo: your work here
-  // generate pallet
+  struct ppm_pixel** image = malloc((size) * sizeof(struct ppm_pixel*));
+  for (int i = 0; i < size; i ++) {
+    image[i] = malloc((size) * sizeof(struct ppm_pixel));
+  }
+  
+  // create a palette containing maxiteration number of colors
   srand(time(0));
+  struct ppm_pixel* palette = malloc((maxIterations) * sizeof(struct ppm_pixel));
+  
+  for (int i = 0; i < maxIterations; i ++) {
+    palette[i].red = rand() % 225;
+    palette[i].green = rand() % 225;
+    palette[i].blue = rand() % 225;
+  }
 
-  // compute image
+  gettimeofday(&tstart, NULL);
+  // calculate the color for each pixel and store
+  for (int i = 0; i < size; i ++) {
+    for (int j = 0; j < size; j ++) {
+      float xfrac = i / (float)size;
+      float yfrac = j / (float)size;
+      float xA = xmin + xfrac * (xmax - xmin);
+      float yA = ymin + yfrac * (ymax - ymin);
+      float x = 0;
+      float y = 0;
+      int iter = 0;
+      while (iter < maxIterations && ((x * x + y * y) < (2*2))) {
+        float xtmp = x * x - y * y + xA;
+        y = 2 * x * y + yA;
+        x = xtmp;
+        iter ++;
+      }
 
+      if (iter < maxIterations) { // escaped
+        // color = palette[iter]
+        image[j][i].blue = palette[iter].blue;
+        image[j][i].green = palette[iter].green;
+        image[j][i].red = palette[iter].red;
+      }
+      else {
+        // color = black
+        image[j][i].blue = 0;
+        image[j][i].green = 0;
+        image[j][i].red = 0;
+      }
+        //escaped
+    }
+  }
+
+  gettimeofday(&tend, NULL);
+  float timer = tend.tv_sec - tstart.tv_sec + (tend.tv_usec - tstart.tv_usec)/1.e6;
+  printf("Computed mandelbrot set (%dx%d) in %.6f seconds\n", size, size, timer);
+  //get the name of the outfile
+
+  char fileName[128];
+  snprintf(fileName, 128, "mandelbrot-%d-%ld.ppm", size, time(0));
+  //print output file
+  printf("Writing file: %s\n", fileName);
+
+  // write the pixels into the output file
+  write_ppm_2d(fileName, image, size, size);
+
+  // free image and palette
+  for (int i = 0; i < size; i ++) {
+    free(image[i]);
+  }
+  free(image);
+
+  free(palette);
+
+  return 0;
+  
 }
--- A08/warmup.c
+++ A08/warmup.c
@@ -1,5 +1,29 @@
+/*----------------------------------------------
+ * Author: Parika Chen
+ * Date: 2023/3/22
+ * This program does a practice with fork()
+ ---------------------------------------------*/
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
 
 int main() {
+  pid_t pid;
+  printf("%d] A0 \n", getpid());
+  pid = fork();
+  if (pid == 0) { // child 1
+    printf("%d] B1 \n", getpid());
+  }
+  else { // parent
+    printf("%d] B0 \n", getpid());
+    pid = fork();
+    if (pid == 0) { // child 2
+      printf("%d] C1 \n", getpid());
+    }
+    else printf("%d] C0 \n", getpid());
+  }
+  printf("%d] Bye \n", getpid());
+
   return 0;
 }
